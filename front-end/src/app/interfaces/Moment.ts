76ea75a2bce40a914d5fa7314d7120931cc3d869
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-16 02:59:09 -0300

    feat: create front-end interfaces

---
+++ front-end/src/app/interfaces/Moment.ts
@@ -0,0 +1,11 @@
+import { Comment } from './Comment';
+
+export interface Moment {
+  id?: string;
+  title: string;
+  description: string;
+  image: string;
+  created_at?: string;
+  updated_at?: string;
+  comments?: Comment[];
+}
