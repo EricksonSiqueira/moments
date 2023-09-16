81abc4dcc19638c1d636bd064907054f01ad37c2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-16 13:03:07 -0300

    feat: create moment form

--- front-end/src/app/components/moment-form/moment-form.component.ts
+++ front-end/src/app/components/moment-form/moment-form.component.ts
@@ -1,10 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from 'src/app/interfaces/Moment';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.scss'],
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Moment>();
   @Input() btnText!: string;
+
+  mommentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.mommentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get title() {
+    return this.mommentForm.get('title')!;
+  }
+
+  get description() {
+    return this.mommentForm.get('description')!;
+  }
+
+  onFileSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+
+    if (fileInput.files && fileInput.files.length > 0) {
+      const file: File = fileInput.files[0];
+
+      this.mommentForm.patchValue({ image: file });
+    }
+  }
+
+  submit() {
+    if (this.mommentForm.invalid) {
+      return;
+    }
+
+    console.log(this.mommentForm.value);
+
+    this.onSubmit.emit(this.mommentForm.value);
+  }
 }
--- front-end/src/app/components/pages/new-moment/new-moment.component.ts
+++ front-end/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Moment } from 'src/app/interfaces/Moment';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class NewMomentComponent {
   btnText = 'Compartilhar!';
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    // enviar para o service
+
+    // exibir mensagem
+
+    // redirect
+  }
 }
